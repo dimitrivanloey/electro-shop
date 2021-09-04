4ff8a06e4891ebe8fc34f5be05ca9a565d52d226
dimitrivanloey <dimitrivanloey@gmail.com>
2021-09-04 20:34:32 +0100

    added functionality to remove items from cart

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -2,8 +2,6 @@ import axios from 'axios'
 import {
   CART_ADD_ITEM,
   CART_REMOVE_ITEM,
-  CART_SAVE_SHIPPING_ADDRESS,
-  CART_SAVE_PAYMENT_METHOD,
 } from '../constants/cartConstants'
 
 export const addToCart = (id, qty) => async (dispatch, getState) => {
@@ -22,4 +20,13 @@ export const addToCart = (id, qty) => async (dispatch, getState) => {
   })
 
   localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems))
+}
+
+export const removeFromCart = (id) => (dispatch, getState) => {
+  dispatch({
+    type: CART_REMOVE_ITEM,
+    payload: id
+  })
+
+  localStorage.setItem('cartItems', JSON.stringify(getState().cartItems))
 }
