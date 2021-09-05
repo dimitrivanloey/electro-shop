045dff3eb1986acd5cca15ff8cdf939e7010bb74
dimitrivanloey <dimitrivanloey@gmail.com>
2021-09-05 11:07:09 +0100

    clean up by using controllers

---
+++ backend/controllers/productController.js
@@ -0,0 +1,32 @@
+import asyncHandler from 'express-async-handler'
+import Product from '../models/productModel.js'
+
+// @desc Fetch all products
+// @route GET /api/products
+// @accesss Public
+const getProducts = asyncHandler(async (req, res) => {
+  const products = await Product.find({})
+
+  res.json(products)
+})
+
+// @desc Fetch single product
+// @route GET /api/products/:id
+// @accesss Public
+const getProductById = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id)
+
+  if (product) {
+    res.json(product)
+  } else {
+    res.status(404)
+    throw new Error('Product not found')
+  }
+
+  res.json(products)
+})
+
+export {
+    getProducts,
+    getProductById
+}
